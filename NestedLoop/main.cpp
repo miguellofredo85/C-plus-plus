c0b59ddb819f7b18b49d9aa294df4a1ce7ec1fe9
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-13 15:48:24 -0300

    nested loop 2d data

--- NestedLoop/main.cpp
+++ NestedLoop/main.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 
 int main(){
-    int i{100};
-        do
-        {
-            if( i % 19 == 0){
-                cout << i << " ";
-                
-            }
-            ++i;
 
-        } while (i <= 1000 );
-        
-    //   std::vector<int> numbers {1,21,4,5,7,3,9,3,5,17,15};
+   const size_t ROW{12}, COL{3};
 
-    // //Don't modify anything above this line
-    // //Your code should go below this line
-    // for(int i : numbers){
-    //     if(i % 3 == 0){
-    //         std::cout << multiple_of_19 << " ";
-    //     }
-    // }
+   cout << "Representing 2d data structure with nested loops\n";
+
+   for(size_t row{0}; row < ROW; ++row){
+    for(size_t col{0}; col < COL; ++col){
+        cout << "row: " << row << " column: " << col << endl;
+    }
+    cout << endl;
+   }
+
+
+   
     return 0;
 }
