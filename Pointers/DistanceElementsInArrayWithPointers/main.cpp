2a59e93fd0d0268f44b0b5c993e6a368fb5ce74a
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-17 16:09:04 -0300

    std_ptrdiff

--- Pointers/DistanceElementsInArrayWithPointers/main.cpp
+++ Pointers/DistanceElementsInArrayWithPointers/main.cpp
@@ -11,7 +11,16 @@ int main(){
     cout << *point1 << endl;
     cout << *point2 << endl;
 
-    cout << *point1 - *point2; // distancia entre elementos
+    cout << *point1 - *point2 << endl; // distancia entre elementos
 
+    //specia std para distancia entre elementos com pointer
+
+    ptrdiff_t positive_pointer = point2 - point1;
+    ptrdiff_t negative_pointer = point1 - point2;
+
+
+    cout << positive_pointer << endl;
+    cout << negative_pointer << endl;
+    cout <<" size of ptrdiff_t " << sizeof(ptrdiff_t)<< endl;
     return 0;
 }
