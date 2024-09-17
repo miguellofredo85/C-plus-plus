f52fada62fce67143e22e307feea3887937207b1
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-17 13:44:42 -0300

    adding info

--- Pointers/PointerArithmeticOperation/main.cpp
+++ Pointers/PointerArithmeticOperation/main.cpp
@@ -11,12 +11,13 @@ int main(){
 
     cout << *point << endl; //1 primer nro do array
     cout << ++point << endl; // move 4 bytes e isso e sumado ao a primera posicao do array entao a resposta sera 5 (posicao 1 + 4 bytes = 5) sem importar o valor do numero da primeira posicao
+    cout << *point << endl; // print 2 pq moveu 4 bytes e agora aponta pra 2 posicao
     point = arr1; // reset the pointer to the start of the array
 
 
     cout << "size fo int = " << sizeof(*point) << endl;
-    
-    cout << *(point+4) << endl; // move forward  4* size of int 
+
+    cout << *(point+4) << endl; // move forward  4* size of int entao printara o 5 que e o que esta na posicao 4 do array
 
 
     point = arr1;
@@ -24,9 +25,22 @@ int main(){
     for (size_t i = 0; i < size(arr1); i++)
     {
         cout << point[i] << " ";
-    }
-    
+             cout << endl;
 
+
+    }
+     cout << "--------------" << endl;
+    
+// modifying data with pointer arithmetic 
+
+arr1[0] = 45;
+*(arr1 + 1) = 33;
+*(point + 2) = 56;
+
+for (size_t i = 0; i < size(arr1); i++)
+    {
+        cout << arr1[i] << " ";
+    }
 
     return 0;
 }
