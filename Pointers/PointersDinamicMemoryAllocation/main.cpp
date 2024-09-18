8e9386da8f4f72b4a2290ea949bdafcce035aaeb
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-18 03:07:22 -0300

    adding info good practices on pointer and solving dangling pointers

--- Pointers/PointersDinamicMemoryAllocation/main.cpp
+++ Pointers/PointersDinamicMemoryAllocation/main.cpp
@@ -25,6 +25,23 @@ point2 = &num2;
 
     delete point; // sempre devemos deletearla luego de su uso para liverar memoria, nunca deletar duas vezes pois podem ocorrer crash do programa ou erros.
 
+    point = nullptr; // reseteamos sempre
+
+    //caso de multiples pointers, sempre deletar o principal
+
+    int* point3{new int{82}};
+    int* point4{point3};
+
+    cout << *point4 << endl;
+
+    delete point3;
+    point3 = nullptr;
+
+    if(!(point3 == nullptr)){
+        cout << "estao tentando escrever a var point3!!";
+    }else{
+        cout << "Terminando o programa corretamente";
+    }
 
     return 0;
 }
