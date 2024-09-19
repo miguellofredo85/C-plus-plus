3f16f9c76528f6f4cc26ebfed8567ec034014c3a
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-19 19:04:29 -0300

    exercise appending

---
+++ CharAndStringManipulation/Exercise/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+ const char input1[] {"Hello there!"};
+    const char input2[] {"How are you?"};
+
+ result.append(str1);
+    result.append(" ");
+    result.append(str2);
+
+return 0;
+}
