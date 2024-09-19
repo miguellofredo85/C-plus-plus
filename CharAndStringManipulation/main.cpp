5c70bc0b87bcbd837c007caff8a72a83f153a68f
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-19 00:37:25 -0300

    eercise with vowel and consonants in array counting

--- CharAndStringManipulation/main.cpp
+++ CharAndStringManipulation/main.cpp
@@ -6,39 +6,61 @@ using namespace std;
 
 int main(){
 
-   cout << "isalnum: ";
-   boolalpha;
+//    cout << "isalnum: ";
+//    boolalpha;
 
-   cout << "C is alphanumeric? : "  << isalnum('C') << '\n';
-   cout << "^ is alphanumeric? : "  << isalnum('^') << '\n';
+//    cout << "C is alphanumeric? : "  << isalnum('C') << '\n';
+//    cout << "^ is alphanumeric? : "  << isalnum('^') << '\n';
 
-   char original_str[]{"Hi my name is Miguel"};
-   char dest_str[(size(original_str))];
+//    char original_str[]{"Hi my name is Miguel"};
+//    char dest_str[(size(original_str))];
 
-   for (size_t i = 0; i < size(original_str); i++)
-   {
-    dest_str[i] = toupper(original_str[i]); // tolower pra fazer o contrario
-   }
+//    for (size_t i = 0; i < size(original_str); i++)
+//    {
+//     dest_str[i] = toupper(original_str[i]); // tolower pra fazer o contrario
+//    }
 
-int blank{};
-  //  for (size_t i = 0; i < size(original_str); i++)
-  //  {
-  //   if(isblank(original_str[i])){
-  //     blank += 1;
-  //   }
-  //  }
+// int blank{};
+//   //  for (size_t i = 0; i < size(original_str); i++)
+//   //  {
+//   //   if(isblank(original_str[i])){
+//   //     blank += 1;
+//   //   }
+//   //  }
    
-  for (auto i : original_str)
-   {
-    if(isblank(i)){
-      blank += 1;
-    }
+//   for (auto i : original_str)
+//    {
+//     if(isblank(i)){
+//       blank += 1;
+//     }
+//    }
+
+
+//   cout << original_str << '\n';
+//   cout << dest_str << '\n';  
+
+// cout << "we found " << blank << " empty spaces" << '\n';
+
+char data[]{"Hi im learning C++"}; unsigned int size[18];
+
+    unsigned int vowel_count{};
+    unsigned int consonant_count{};
+    
+    //Don't modify anything above this line
+    //Your code should go below this line
+    for(size_t i = 0; i < size; i++){
+
+       if(data[i] == 'a' || data[i] == 'e' || data[i] == 'i' || data[i] == 'o' || data[i] == 'u' || data[i] == 'A' || data[i] == 'E' || data[i] == 'I' || data[i] == 'O' || data[i] == 'U' ){
+
+           vowel_count += 1;
+
+       }else if( ( (data[i] != 'a') || (data[i] != 'e') || (data[i] != 'i') || (data[i] != 'o') || (data[i] != 'u') || (data[i] != 'A') || (data[i] != 'E') || (data[i] != 'I') || (data[i] != 'O') || (data[i] != 'U')) && !(isdigit(data[i])) && !(isxdigit(data[i])) && !(isspace(data[i])) && !(isblank(data[i])) && !(ispunct(data[i])) ){
+
+           consonant_count += 1;
+
+       }
    }
-
-
-  cout << original_str << '\n';
-  cout << dest_str << '\n';  
-
-cout << "we found " << blank << " empty spaces" << '\n';
+   
+   std::cout << "The string contains "<< vowel_count << " vowels and " << consonant_count << " consonants";
 return 0;
 }
