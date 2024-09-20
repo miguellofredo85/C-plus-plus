f9af60eed636571498d46eba27c3491f3f0de832
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-20 01:30:42 -0300

    empty shrink capacity

--- CharAndStringManipulation/AccessingCharInString/main.cpp
+++ CharAndStringManipulation/AccessingCharInString/main.cpp
@@ -42,10 +42,36 @@ const char* point = line.c_str(); // this methos will make my var line const, so
 
 // point[0] = 'E'; //assignment of read-only location '* point'
 
+cout << line << endl;
 
 
 
-cout << line;
+string um {"give me give me"};
+string dois;
+string tres{};
+
+cout << um.size() << " ";
+cout << dois.size() << " ";
+cout << tres.size() << endl;
+
+cout << "===========" << endl;
+
+cout << um.length() << " ";
+cout << dois.length() << " ";
+cout << tres.length() << endl;
+
+cout << "===========" << endl;
+
+cout << boolalpha << um.empty() << " ";
+cout << boolalpha << dois.empty() << " ";
+cout << boolalpha << tres.empty() << endl;
+
+cout << um.capacity() << endl;
+um.reserve(100);
+cout << um.capacity() << endl;
+
+um.shrink_to_fit(); // fit the capacity / size to the total of chars in the string
+cout << um.capacity() << endl;
 
 
 
