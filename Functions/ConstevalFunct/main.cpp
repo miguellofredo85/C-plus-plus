5f5d1f133c5c018347d37454b62a54849f49b165
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-23 15:12:39 -0300

    adding info

--- Functions/ConstevalFunct/main.cpp
+++ Functions/ConstevalFunct/main.cpp
@@ -14,7 +14,7 @@ int main(){
    std::cout << "num1: " << num1 << std::endl;
 
     int num2{9}; // essa var e evaluada in run time entao dara erro ao chamar a funct emvaixo pois ela ao ser consteval sera evaluada at compile time
-   std::cout << "num1: " << multi(num2) << std::endl;
+   std::cout << "num1: " << multi(num2) << std::endl; // error
 
 return 0;
 }
