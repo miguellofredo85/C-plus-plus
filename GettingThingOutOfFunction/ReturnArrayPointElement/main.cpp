63773b3d8c888a6b0e055050da6cc1f6cdfaf50c
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-24 14:33:41 -0300

    adding excercise max num negative from array with pointer function

--- GettingThingOutOfFunction/ReturnArrayPointElement/main.cpp
+++ GettingThingOutOfFunction/ReturnArrayPointElement/main.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 
-const double* max_index_array(const double arr[], int sizearr);
 
+ const double* find_max_address(const double scores[], unsigned int count){
 
-const double* max_index_array(const double arr[], int sizearr){ // funct must const so cant modify arr
+    unsigned int max_index{};
+    double max{scores[0]};
+    
+    for(unsigned int i{0}; i < count ; ++i){
+        if(scores[i] > max){
+            max = scores[i];
+            max_index = i;
+        }
 
-int store_max_i{};
-double max{};
-
-for (size_t i = 0; i < sizearr; i++){
-    if(arr[i] > max){
-        max = arr[i];
-        store_max_i = i;
     }
+    return &scores[max_index];
 }
-std::cout << store_max_i << std::endl;
-return &arr[store_max_i]; 
 
-}
+// const double* max_index_array(const double arr[], int sizearr);
+
+
+// const double* max_index_array(const double arr[], int sizearr){ // funct must const so cant modify arr
+
+// int store_max_i{};
+// double max{};
+
+// for (size_t i = 0; i < sizearr; i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//         store_max_i = i;
+//     }
+// }
+// std::cout << store_max_i << std::endl;
+// return &arr[store_max_i]; 
+
+// }
 
 int main(){
 
-double ar[]{2.0, 6.6, 1.4, 8.9, 4.5,3.2};
+// double ar[]{2.0, 6.6, 1.4, 8.9, 4.5,3.2};
 
-max_index_array(ar, std::size(ar)); // op1
+double ar[]{-3.0,-2.0,-5.0};
+const double* max_index = find_max_address(ar, std::size(ar));
+// max_index_array(ar, std::size(ar)); // op1
 
-const double* max_index = max_index_array(ar, std::size(ar)); // 3, for position of 8.9 op2
+// const double* max_index = max_index_array(ar, std::size(ar)); // 3, for position of 8.9 op2
 
 std:: cout << *max_index; // 8,9
    
