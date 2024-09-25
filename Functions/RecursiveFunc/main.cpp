72ed019bb46c5ff04c4e50dfee23dffcbe2fbe78
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-25 20:38:47 -0300

    adding factorial excercise

--- Functions/RecursiveFunc/main.cpp
+++ Functions/RecursiveFunc/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 
-size_t sum_up_to_zero(size_t value){
-    if(value!=0)
-        return value + sum_up_to_zero(value-1);
-    return 0;
+// size_t sum_up_to_zero(size_t value){
+//     if(value!=0)
+//         return value + sum_up_to_zero(value-1);
+//     return 0;
+// }
+
+unsigned int factorial(unsigned int n){
+
+    if(n > 1){
+
+    n *= (factorial(n-1));
+    }
+
+    
+
+    return n;
+    
 }
 
 int main(){
 
-    std::cout << "result : " << sum_up_to_zero(10) << std::endl;
-   
+    // std::cout << "result : " << sum_up_to_zero(10) << std::endl;
+
+
+   std::cout << factorial(5); 
+
     return 0;
 }
