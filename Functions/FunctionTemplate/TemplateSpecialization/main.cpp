d0c62e8a78a971b8cbbed04a1ee5e37fba6c7358
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-26 17:02:04 -0300

    adding exercise with template specialization

--- Functions/FunctionTemplate/TemplateSpecialization/main.cpp
+++ Functions/FunctionTemplate/TemplateSpecialization/main.cpp
@@ -2,18 +2,29 @@
 #include <iostream>
 #include <cstring>
 
-template <typename T> T maximum(T a,T b){
-    return (a > b) ? a : b ; 
-}
+// template <typename T> T maximum(T a,T b){
+//     return (a > b) ? a : b ; 
+// }
 
 
-template <>
-const char * maximum<const char*> (const char* a, const char* b){
-    return ( std::strcmp(a,b) > 0) ? a : b;
-}
+// template <>
+// const char * maximum<const char*> (const char* a, const char* b){
+//     return ( std::strcmp(a,b) > 0) ? a : b;
+// }
+template<typename T>
+int hunt_down( T value,  T* collection, unsigned int size);
+
+
+template<>
+int hunt_down(const char* value,  const char* collection[], unsigned int size);
+
 
 
 int main(){
+
+	  unsigned int student_id{ 817616 };
+    unsigned int student_ids[]{ 111222,442222,817616,333900 };
+    int index{ hunt_down(student_id, student_ids, 4) }; //
 
     /*
 	int a{10};
@@ -33,13 +44,42 @@ int main(){
     */
 
     //
-	const char* g{"wild"};
-	const char* h{"animal"};
+	// const char* g{"wild"};
+	// const char* h{"animal"};
 	
-	//This won't do what you would expect : BEWARE!
+	// //This won't do what you would expect : BEWARE!
 
-    const char* result = maximum(g,h);
-	std::cout << "max(const char*) : " << result << std::endl;
+  //   const char* result = maximum(g,h);
+	// std::cout << "max(const char*) : " << result << std::endl;
    
     return 0;
+}
+
+
+template <typename T>
+int hunt_down( T value,  T* collection, unsigned int size)
+{
+	for (unsigned int i{ 0 }; i < size; i++) {
+		if (collection[i] == value) {
+			return i; // Found it; return the index.
+		}
+	}
+	return -1; 
+}
+
+
+template<>
+int hunt_down(const char* value,  const char* collection[], unsigned int size){
+
+
+	for (unsigned int i{ 0 }; i < size; i++) {
+		if (std::strcmp(value, collection[i]) == 0) {
+			std::cout << i;
+			return i; 
+		}
+	}
+
+	std::cout << -1;
+	return -1; 
+
 }
