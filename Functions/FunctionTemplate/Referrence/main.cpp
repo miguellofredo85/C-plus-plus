e1a5a89d1c05c7d5804acec8b9d03525c31841d6
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-26 15:11:27 -0300

    adding exercise swap nums with template and references

--- Functions/FunctionTemplate/Referrence/main.cpp
+++ Functions/FunctionTemplate/Referrence/main.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
+using namespace std;
 
-template <typename T> const T& maximum(const T& a, const T& b); // Declaration
+template<typename T> void swap_data(T& a, T& c){
+    T temp = a;
+    a = c;
+    c = temp;
+    cout << a << endl;
+    cout << c << endl;
+
+}
+// template <typename T> const T& maximum(const T& a, const T& b); // Declaration
 //template <typename T> T maximum(T a, T b); // Declaration
 
 int main(){
 
-    double a {23.5};
-    double b {51.2};
+    string e{"Hello"};
+    string r{"There"};
 
-    std::cout << "Out - &a: " << &a << std::endl; // 0x111abc
-    auto result = maximum(a,b);
-    std::cout << "Out - &a: " << &a << std::endl; // 0x111abc
+     int x{10};
+    int y{20};
+    swap_data(x,y);
+    cout << x << " " << y; 
+
+    // double a {23.5};
+    // double b {51.2};
+
+    // std::cout << "Out - &a: " << &a << std::endl; // 0x111abc
+    // auto result = maximum(a,b);
+    // std::cout << "Out - &a: " << &a << std::endl; // 0x111abc
   
     return 0;
 }
 
 
-//Definition
-template <typename T> const T& maximum(const T& a, const T& b){
-    std::cout << "In - &a: " << &a << std::endl; // // 0x111abc
-    return (a > b ) ? a : b ;
-}
+// //Definition
+// template <typename T> const T& maximum(const T& a, const T& b){
+//     std::cout << "In - &a: " << &a << std::endl; // // 0x111abc
+//     return (a > b ) ? a : b ;
+// }
 
 /*
 template <typename T> T maximum(T a, T b){
@@ -28,3 +45,4 @@ template <typename T> T maximum(T a, T b){
     return (a > b ) ? a : b ;
 }
 */
+
