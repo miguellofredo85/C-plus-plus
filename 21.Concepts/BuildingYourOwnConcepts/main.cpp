19910cb6cd954267f7fd844527dbd4f8810fdd4b
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-28 00:37:13 -0300

    adding exercise concat nums with own concepts

--- 21.Concepts/BuildingYourOwnConcepts/main.cpp
+++ 21.Concepts/BuildingYourOwnConcepts/main.cpp
@@ -1,49 +1,61 @@
 #include <iostream>
 #include <type_traits>
 #include <concepts>
+#include <string>
 
-//Syntax1
-/*
-template <typename T>
-concept MyIntegral = std::is_integral_v<T>;
+// //Syntax1
+// /*
+// template <typename T>
+// concept MyIntegral = std::is_integral_v<T>;
 
-MyIntegral auto add( MyIntegral auto a, MyIntegral auto b) {
-    return a + b;
-}
-*/
+// MyIntegral auto add( MyIntegral auto a, MyIntegral auto b) {
+//     return a + b;
+// }
+// */
 
 
-template <typename T>
-concept Multipliable =  requires(T a, T b) {
-	a * b; // Just makes sure the syntax is valid
+// template <typename T>
+// concept Multipliable =  requires(T a, T b) {
+// 	a * b; // Just makes sure the syntax is valid
+// };
+
+
+// template <typename T>
+// concept Incrementable = requires (T a) {
+// 	a+=1;
+// 	++a;
+// 	a++;
+// };
+
+// template <typename T>
+// requires Incrementable<T>
+// T add (T a, T b){
+//     return a + b;
+// }
+
+
+
+template<typename T>
+concept Tostring = requires(T a){
+    std::to_string(a);
 };
 
-
-template <typename T>
-concept Incrementable = requires (T a) {
-	a+=1;
-	++a;
-	a++;
-};
-
-template <typename T>
-requires Incrementable<T>
-T add (T a, T b){
-    return a + b;
+std::string concatenate(Tostring auto a, Tostring auto c){
+    return std::to_string(a) + std::to_string(c);
 }
-
 
 int main(){
 
-    double x{6};
-    double y{7};
+    int x{65};
+    int y{8767};
 
-    //std::string x{"Hello"};
-    //std::string y{"World"};
+    // std::string x{"Hello"};
+    // std::string y{"World"};
+auto result = concatenate(x,y);
 
-    add(x,y);
-
-    std::cout << "Done!" << std::endl;
+std::cout << "result : " << result << std::endl;
+    // add(x,y);
+    // std::cout << "Done!" << std::endl;
     
     return 0;
 }
