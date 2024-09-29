5ca454ce5f57181d4246a079c8daf4b37040189d
Miguel Lofredo <miguellofredo@gmail.com>
2024-09-29 12:52:55 -0300

    remove } XD

--- 22.Classes/ClassAcrossMultiplesFiles/cylinder.h
+++ 22.Classes/ClassAcrossMultiplesFiles/cylinder.h
@@ -17,7 +17,7 @@ class Cylinder {
         double get_base_radius();
         double get_height();
         void set_base_radius(double rad_param);
-        void set_height(double height_param){;
+        void set_height(double height_param);
 
     private : 
         //Member variables
